9292b6dc1c04f13ea4e110bce5b6203110f9e824
Max Mohammadi <maxmohammadi@gmail.com>
2021-05-17 18:36:07 -0700

    add box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Canvas } from "react-three-fiber"
+
+function Box(props) {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  return (
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      ref={ref}
+      position={[0, 2, 0]}
+    >
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
 
 function App() {
-
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-
   return (
-    <main>
-      <h1>Max Mohammadi</h1>
-      <h2>Portfolio</h2>
-    </main>
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
   );
 }
 
