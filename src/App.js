af7aa7fd1d6fc0ea9d84824a5e29734cfc314441
Max Mohammadi <maxmohammadi@gmail.com>
2021-06-02 23:37:07 -0700

    non working apex charts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { useRef, useMemo, useState } from 'react'
+import React, { useRef, useMemo, useState, Component } from 'react'
 import * as THREE from '@react-three/fiber'
+import Donut from './components/Donut'
 
 function App() {
   return (
-    <h1>hey</h1>
+    <Donut />
   );
 }
 
