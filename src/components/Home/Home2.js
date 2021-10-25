d51b495543bdaf442ab3753d06fa4b7cb2b065b5
Max Mohammadi <maxmohammadi@gmail.com>
2021-10-24 19:46:45 -0700

    fix bug

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -90,7 +90,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                  href="https://medium.com/@maxmohammadi"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
